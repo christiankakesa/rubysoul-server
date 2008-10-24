3a93d1c69361d726e26bc56c65c782086fb5c7df
christian.kakesa <christian.kakesa@30f90d39-204a-0410-8766-b92f99aaaa30>
2008-10-24 08:24:52 +0000

    Remove line feed and null character at the end

--- lib/kerberos/kerberos.c
+++ lib/kerberos/kerberos.c
@@ -179,11 +179,9 @@ unsigned char * base64_encode(const unsigned char *src, size_t len, size_t *out_
 	const unsigned char *end, *in;
 	size_t olen;
 	int line_len;
-	const unsigned char *base64_table = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+	const unsigned char base64_table[65] = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
 	olen = len * 4 / 3 + 4; /* 3-byte blocks to 4-byte */
-	olen += olen / 72; /* line feeds */
-	olen++; /* nul termination */
 	out = malloc(olen);
 	if (out == NULL)
 		return NULL;
@@ -191,18 +189,12 @@ unsigned char * base64_encode(const unsigned char *src, size_t len, size_t *out_
 	end = src + len;
 	in = src;
 	pos = out;
-	line_len = 0;
 	while (end - in >= 3) {
 		*pos++ = base64_table[in[0] >> 2];
 		*pos++ = base64_table[((in[0] & 0x03) << 4) | (in[1] >> 4)];
 		*pos++ = base64_table[((in[1] & 0x0f) << 2) | (in[2] >> 6)];
 		*pos++ = base64_table[in[2] & 0x3f];
 		in += 3;
-		line_len += 4;
-		if (line_len >= 72) {
-			*pos++ = '\n';
-			line_len = 0;
-		}
 	}
 
 	if (end - in) {
@@ -219,10 +211,6 @@ unsigned char * base64_encode(const unsigned char *src, size_t len, size_t *out_
 		line_len += 4;
 	}
 
-	if (line_len)
-		*pos++ = '\n';
-
-	*pos = '\0';
 	if (out_len)
 		*out_len = pos - out;
 	return out;
