0bb18a6f1cf9fecf76dd9ed568ef07681213d030
christian.kakesa <christian.kakesa@30f90d39-204a-0410-8766-b92f99aaaa30>
2009-05-28 22:39:58 +0000

    Better socket handling. Works with ruby 1.9

--- rubysoul-server.rb
+++ rubysoul-server.rb
@@ -5,15 +5,11 @@ rescue LoadError
   STDERR.puts "Error: #{$!}"
   exit
 end
-
-trap("SIGINT") { exit }
-trap("SIGTERM") { exit }
     
 begin
   rss = NetsoulServer.new(ARGV)
 rescue
   STDERR.puts "[#{Time.now.to_s}] Error: #{$!}"
-  sleep 5
   retry
 end
 
