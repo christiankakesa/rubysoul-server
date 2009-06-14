164aa49c79dbf1c1b2dad06781a8ff41c541501d
christian.kakesa <christian.kakesa@30f90d39-204a-0410-8766-b92f99aaaa30>
2009-06-14 00:14:47 +0000

    Fixe reactor warnings

--- lib/reactor.rb
+++ lib/reactor.rb
@@ -182,7 +182,7 @@ module Reactor
     
     def fire_ios(mode, ios)
       ios.each do |io|
-        if io.respond_to? (mode == :read ? :notify_readable : :notify_writable)
+        if io.respond_to?(mode == :read ? :notify_readable : :notify_writable)
           io.__send__((mode == :read ? :notify_readable : :notify_writable), self)
         else
           @selectables[mode][:callbacks][io.object_id].call(io, self) if @selectables[mode][:callbacks][io.object_id]
